94c5b26c81ccfc311e0ed5d10f4c062878ff2e5e
Matti Airas <matti.airas@hatlabs.fi>
2023-09-27 19:51:17 +0300

    Clean up the web page compressing script
    Also make it use the updated src paths

--- extra_script.py
+++ extra_script.py
@@ -1,49 +1,39 @@
-Import("env")
+Import('env')
 import gzip
+from typing import Tuple
 
-def make_c_header(inName, outName):
+def make_c_header(in_name: str, out_name: str) -> None:
 
-   print('Writing ',inName,' to src/sensesp/net/web/',outName,'.h')
+   print('Writing ', in_name, ' to src/sensesp/net/web/static/', out_name, '.h')
 
-   infile = open('web/docroot/' + inName, "rb")
-   inFileBytes = infile.read()
-   print('Compressing into gzip...')
-   inFileGziped = gzip.compress(inFileBytes)
-   print('Non compressed size is ', str(len(inFileBytes)), ", gziped size is ", str(len(inFileGziped)), ".")
-   outfile = open("src/sensesp/net/web/" + outName + ".h","w") 
-   print('Generating header file...')
-   outfile.write("#include <pgmspace.h>\n")
-   outfile.write("const uint8_t PAGE_")
-   outfile.write(outName)
-   outfile.write("[] PROGMEM = {\n\t\t")
-   lineBreak = 7
-   for b in inFileGziped:
-      outfile.write(hex(b))
-      outfile.write(",")
-      if lineBreak == 0:
-         outfile.write("\n\t\t")
-         lineBreak = 8
-      lineBreak-=1
+   with open('web/docroot/' + in_name, "rb") as infile:
+      in_file_bytes = infile.read()
+      print('Compressing into gzip...')
+      in_file_gzipped = gzip.compress(in_file_bytes)
+      print('Non compressed size is ', str(len(in_file_bytes)), ", gzipped size is ", str(len(in_file_gzipped)), ".")
+      with open("src/sensesp/net/web/static/" + out_name + ".h","w") as outfile:
+         print('Generating header file...')
+         outfile.write("#include <pgmspace.h>\n")
+         outfile.write("const char PAGE_")
+         outfile.write(out_name)
+         outfile.write("[] = {\n\t\t")
+         line_break = 7
+         for b in in_file_gzipped:
+            outfile.write(hex(b))
+            outfile.write(",")
+            if line_break == 0:
+               outfile.write("\n\t\t")
+               line_break = 8
+            line_break -= 1
 
-   #for line in infile:
-   #   outfile.write(line)
+         outfile.write("};\n\n")
+         #const uint PAGE_index_size = 8;
 
-   outfile.write("};\n\n")
-   #const uint PAGE_index_size = 8;
-   outfile.write("const uint PAGE_")
-   outfile.write(outName)
-   outfile.write("_size = ")
-   outfile.write(str(len(inFileGziped)))
-   outfile.write(";\n")
-   infile.close()
-   outfile.close()
-
-
-def build_webUI(*args, **kwargs):
+def build_web_ui(*args, **kwargs) -> None:
    env.Execute("terser --compress --output web/docroot/js/sensesp.min.js -- web/docroot/js/sensesp.js")
    make_c_header("js/sensesp.min.js", "js_sensesp")
    make_c_header("js/jsoneditor.min.js", "js_jsoneditor")
    make_c_header("css/bootstrap.min.css", "css_bootstrap")
    make_c_header("index.html", "index")
 
-env.AlwaysBuild(env.Alias("webUI", None, build_webUI))
+env.AlwaysBuild(env.Alias("webUI", None, build_web_ui))
