27d956c4c2338b542522a4846bc1b4f2a1233424
Matti Airas <matti.airas@hatlabs.fi>
2023-10-01 22:32:28 +0300

    Fix web project configuration

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
+import mockDevServerPlugin from 'vite-plugin-mock-dev-server'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [
+    preact(),
+    mockDevServerPlugin(
+      {
+        //prefix: '^/config',
+        log: 'debug',
+        //include: 'mock/**/*.mock.{ts,js,cjs,mjs,json,json5}',
+      }
+    ),
+
+  ],
+  server: {
+    proxy: {
+      '^/config': { target: 'http://example.com' },
+      '^/info': { target: 'http://example.com' }
+    }
+  }
 });
