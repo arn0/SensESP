f19bc5594ea24c6a6b7ad8ede37c7764c4ec8bf6
Matti Airas <matti.airas@hatlabs.fi>
2023-10-05 11:10:09 +0300

    Add missing bootstrap imports

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,7 +3,8 @@ import "preact/debug"; // <-- Add this line at the top of your main entry file
 import { render } from "preact";
 import { App } from "./components/App";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js"
 import "./style.css";
 
 render(<App />, document.getElementById("app"));
