ecc83f60c3ccdbd034f488cbdd885987cc47783d
Matti Airas <matti.airas@hatlabs.fi>
2023-10-05 11:05:16 +0300

    Create an app config file

---
+++ web/src/app_config.ts
@@ -0,0 +1,8 @@
+
+export const app_config = {
+  // leave empty if the API is on the same host as the web app
+  api_host_url: '',
+  config_path: '/api/config',
+  info_path: '/api/info',
+  signalk_path: '/api/signalk',
+};
