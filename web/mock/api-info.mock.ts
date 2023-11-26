ee2b386fc9e21424c063752e7d13cf3029b9310b
Matti Airas <matti.airas@hatlabs.fi>
2023-11-26 22:21:51 +0200

    Update the info Json format

--- web/mock/api-info.mock.ts
+++ web/mock/api-info.mock.ts
@@ -7,74 +7,73 @@ export default defineMock(
       delay: 800,
       body: () => {
 
-        const data = {
-          items: [
-            {
-              "name": "Build date",
-              "value": "Apr 29 2022 12:34:26",
-              "group": "Software",
-              "order": 2000
-            },
-            {
-              "name": "Hostname",
-              "value": "asyncnew",
-              "group": "Network",
-              "order": 500
-            },
-            {
-              "name": "MAC Address",
-              "value": "94:3C:C6:9A:xx:xx",
-              "group": "Network",
-              "order": 1100
-            },
-            {
-              "name": "SK connection status",
-              "value": "Connected",
-              "group": "Network",
-              "order": 1600
-            },
-            {
-              "name": "SSID",
-              "value": "Sensors",
-              "group": "Network",
-              "order": 1200
-            },
-            {
-              "name": "SenseESP version",
-              "value": "2.4.2-alpha",
-              "group": "Software",
-              "order": 1900
-            },
-            {
-              "name": "Signal K server address",
-              "value": "",
-              "group": "Network",
-              "order": 1400
-            },
-            {
-              "name": "Signal K server port",
-              "value": 0,
-              "group": "Network",
-              "order": 1500
-            },
-            {
-              "name": "WiFi signal strength",
-              "value": -45,
-              "group": "Network",
-              "order": 1300
-            }
-          ]
-        }
+        const data = [
+          {
+            "name": "Build date",
+            "value": "Apr 29 2022 12:34:26",
+            "group": "Software",
+            "order": 2000
+          },
+          {
+            "name": "Hostname",
+            "value": "asyncnew",
+            "group": "Network",
+            "order": 500
+          },
+          {
+            "name": "MAC Address",
+            "value": "94:3C:C6:9A:xx:xx",
+            "group": "Network",
+            "order": 1100
+          },
+          {
+            "name": "SK connection status",
+            "value": "Connected",
+            "group": "Network",
+            "order": 1600
+          },
+          {
+            "name": "SSID",
+            "value": "Sensors",
+            "group": "Network",
+            "order": 1200
+          },
+          {
+            "name": "SenseESP version",
+            "value": "2.4.2-alpha",
+            "group": "Software",
+            "order": 1900
+          },
+          {
+            "name": "Signal K server address",
+            "value": "",
+            "group": "Network",
+            "order": 1400
+          },
+          {
+            "name": "Signal K server port",
+            "value": 0,
+            "group": "Network",
+            "order": 1500
+          },
+          {
+            "name": "WiFi signal strength",
+            "value": -45,
+            "group": "Network",
+            "order": 1300
+          }
+        ];
+
         // get a random integer between 0 and 100
         const randomInteger = Math.floor(Math.random() * 100);
 
-        data.items.push({
+        data.push({
           "name": "Battery level",
           "value": randomInteger,
           "group": "Power",
           "order": 100
         })
-        return data
+        return data;
       }
     }
   ]
